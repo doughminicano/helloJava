d4356017ad5df9db1de647564b2cab222abec226
Daniel Garcia <73608127+doughminicano@users.noreply.github.com>
2021-09-08 12:15:50 -0400

    Create HelloJava.java

---
+++ HelloJava.java
@@ -0,0 +1,14 @@
+public class HelloJava {
+    String name = "Danny";
+    int age = 32;
+    String hometown = "Jacksonville";
+
+    public String introduction() {
+        return "Hello! My name is " + name +" and I am " + age + " years old";
+    }
+
+    public String secondIntro(String name2, int age2) {
+                return "Hello! My name is " + name2 +" and I am " + age2 + " years old";
+    }
+
+}
