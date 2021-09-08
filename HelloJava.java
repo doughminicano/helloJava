2eae6c856026bf45eb07114e1b83b9ae12792930
Daniel Garcia <73608127+doughminicano@users.noreply.github.com>
2021-09-08 12:24:43 -0400

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,14 +1,8 @@
-public class HelloJava {
-    String name = "Danny";
-    int age = 32;
-    String hometown = "Jacksonville";
-
-    public String introduction() {
-        return "Hello! My name is " + name +" and I am " + age + " years old";
-    }
-
-    public String secondIntro(String name2, int age2) {
-                return "Hello! My name is " + name2 +" and I am " + age2 + " years old";
-    }
+public class static void main(String[], args) {
+    HolaJava();
+   
+public String HolaJava() {
+    System.out.println("Hello Java World!!");
+}
 
 }
