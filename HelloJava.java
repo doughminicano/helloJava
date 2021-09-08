c5b1d0bb11e22685626fde14c35c6154119e23bd
Daniel Garcia <73608127+doughminicano@users.noreply.github.com>
2021-09-08 12:30:28 -0400

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,8 +1,6 @@
+class HelloJava {
 public class static void main(String[], args) {
-    HolaJava();
-   
-public String HolaJava() {
+    
     System.out.println("Hello Java World!!");
-}
-
+    }
 }
